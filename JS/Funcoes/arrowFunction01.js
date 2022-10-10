940261829bc26fd45de56005434b6be654207276
Fernando José Rodrigues <lkgfernando@gmail.com>
2022-10-10 08:19:03 -0300

    #

---
+++ JS/Funcoes/arrowFunction01.js
@@ -0,0 +1,20 @@
+let dobro = function(a) {
+    return 5 * a
+}
+
+console.log(dobro(5))
+
+dobro = (a) => {
+    return 2 * a
+}
+
+console.log(dobro(10))
+
+
+dobro = a => 3 * a
+
+console.log(dobro(3))
+
+ola = () => 'Olá'
+
+console.log(ola())
