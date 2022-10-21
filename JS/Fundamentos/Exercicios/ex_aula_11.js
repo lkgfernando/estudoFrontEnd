81063feff3b6ee5d5cc7d777f90b6c0712109734
Fernando José Rodrigues <lkgfernando@gmail.com>
2022-10-21 15:56:35 -0300

    #

---
+++ JS/Fundamentos/Exercicios/ex_aula_11.js
@@ -0,0 +1,6 @@
+const num1 = prompt("Digite o valor do premeiro numero: ")
+const num2 = prompt("Digite o valor do segundo numero: ")
+let somaNumeros = Number(num1) + Number(num2)
+
+
+alert(`A soma entre o Num1 e Num2 é ${somaNumeros}`)
---
+++ JS/Fundamentos/Exercicios/troca_variaveis.js
@@ -0,0 +1,11 @@
+let cA = 'A'
+let cB = 'B'
+let cC = 'C'
+let cX = ''
+
+cX = cA
+cA = cB
+cB = cC
+cC = cX
+
+console.log(cA, cB, cC)
