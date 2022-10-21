c0001d6b9b3fc0c0e3c0913394d231df507618f9
Fernando José Rodrigues <lkgfernando@gmail.com>
2022-10-21 10:03:50 -0300

    Exercicios de Js

---
+++ JS/Fundamentos/Exercicios/console.js
@@ -0,0 +1,2 @@
+let num = 10
+console.log(`Meu nome é "Fernando". Estou aprendendo JavaScript ás ${num} da manhã´`)
---
+++ JS/Fundamentos/Exercicios/imc.js
@@ -0,0 +1,12 @@
+const altura = 1.75
+const peso = 107
+const idade = 37 
+let imc 
+let anoNascimento
+
+
+imc = peso / (altura * altura)
+anoNascimento = 2022 - idade 
+
+console.log(imc)
+console.log(anoNascimento)
