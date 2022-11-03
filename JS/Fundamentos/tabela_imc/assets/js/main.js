6efbf4d940283fa6d407240505f7aaaa1b6290ab
Fernando José Rodrigues <lkgfernando@gmail.com>
2022-11-03 16:58:32 -0300

    Imc

--- JS/Fundamentos/tabela_imc/assets/js/main.js
+++ JS/Fundamentos/tabela_imc/assets/js/main.js
@@ -1,33 +1,84 @@
 function calcImcMain() {
     const form = document.querySelector('.calc-imc')
-    const calc = document.querySelector('.calc')
-
-    const amrImc = []
-
-    function calcImc(weight, height) {
-        return  weight / height
-    }
+     
 
     function receberEventos (evento) {
-        evento.preventDefaut()
+        evento.preventDefault()
+        const imc_weight = evento.target.querySelector('#input-weight')
+        const imc_height = evento.target.querySelector('#input-height')
+        const weight  = Number(imc_weight.value)
+        const height = Number(imc_height.value)
+        const imc = getImc(weight, height)
+        const nivelImc = getNivelImc(imc)
+        const msg = `Seu IMC é ${imc} (${nivelImc})`
 
-        const imc_weight = document.querySelector('#input-weight')
-        const imc_height = document.querySelector('#input-height')
+        if (!weight) {
+            setResult('Peso inválido', false)
+            return
+        }
 
+        if (!height) {
+            setResult('Altura inválido', false)
+            return
+        }
+        
 
-        amrImc.push({
-            peso: imc_weight.value,
-            altura: imc_height.value
-        })
+        setResult(msg, true)
+        
 
-         
-       calc.innerHTML +=  `<p>${calcImc(peso.value, altura.value)}</p>`
+    } 
+
+    function getNivelImc (imc) {
+        const level = ['Abaixo do peso', 'Peoso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+
+        if(imc >= 39.9) {
+            return level[5]
+        } else if (imc >= 34.9) {
+            return level[4]
+        } else if (imc >= 29.9) {
+            return level[3]
+        } else if (imc >= 24.9) {
+            return level[2]
+        } else if (imc >= 18.5) {
+            return level[1]
+        } else if (imc < 18.5) {
+            return level[0]
+        }
 
     }
+    
+    
+    function getImc(weight, height) {
+        const imc = weight / height ** 2
+        return imc.toFixed(2)
+    }
+
+    function setResult (msg, isValid) {
+        const calc = document.querySelector('.calc')
+        calc.innerHTML = ''
+        // Cria HTML
+        const p = criaP()
+
+        if (isValid) {
+            p.classList.add('result')
+        } else {
+            p.classList.add('bad')
+        }
+
+
+        p.innerHTML = msg
+        calc.appendChild(p)
+
+    }
+
+    function criaP() {
+        const p = document.createElement('p')
+        return p
+    }
+    
 
     form.addEventListener('submit', receberEventos)
 
 }
-
 
 calcImcMain()
