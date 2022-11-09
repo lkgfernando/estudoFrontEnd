c057c583e23a87ba4c9f88096c9f66992b37bc4a
Fernando José Rodrigues <lkgfernando@gmail.com>
2022-11-09 16:57:29 -0300

    #

--- JS/Fundamentos/Exercicios/fizzBuzz.js
+++ JS/Fundamentos/Exercicios/fizzBuzz.js
@@ -4,11 +4,11 @@ function fizzBuzz (number) {
     for(let i = 0 ; i < number ; i++) {
         numbers.push(i)
 
-        if (numbers[i] % 3 === 0) {
+        if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
             console.log('Fizz')
-        } else if (numbers[i] % 5 === 0) {
+        } else if  (numbers[i] % 3 === 0) {
             console.log('Buzz')
-        } else if (numbers[i] % 3 === 0 && numbers[i] % 5 === 0) {
+        } else if (numbers[i] % 5 === 0) {
             console.log('FizzBuzz')
         } else {
             console.log(numbers[i])
