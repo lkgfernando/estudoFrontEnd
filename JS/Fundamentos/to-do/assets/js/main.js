3ce59a24db3fb8b8e8b48f4fe413a479842254b7
Fernando José Rodrigues <lkgfernando@gmail.com>
2022-11-14 14:43:28 -0300

    to-do

--- JS/Fundamentos/to-do/assets/js/main.js
+++ JS/Fundamentos/to-do/assets/js/main.js
@@ -2,22 +2,51 @@
 const inputTask = document.querySelector('.input-task')
 const btnTask = document.querySelector('#btn-task')
 const btnClear = document.querySelector('#btn-clear')
-const addTask = document.querySelector('.add-task')
+const addTask = document.querySelector('.list-group')
 
 
 // #################### Functions ##############################
 
-// Cria tag li
+// Cria tags
 function createLi() {
     const li = document.createElement('li')
+    li.classList.add('list-group-item')
+    li.classList.add('d-flex')
+    li.classList.add('justify-content-between')
+    li.classList.add('align-items-center')
+    li.classList.add('card-text')
     return li
 }
+
+function createButtonDelete(li) {
+    li.innerHTML += ' '
+    const buttonDelete = document.createElement('button')
+    buttonDelete.classList.add('btn')
+    buttonDelete.classList.add('btn-outline-secondary')
+    buttonDelete.classList.add('delete-task')
+    buttonDelete.innerText = 'Excluir'
+    li.appendChild(buttonDelete)
+}
+
+
+
+//Captura teclar precionada no teclado
+
+inputTask.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        if (!inputTask.value) return
+        createTask(inputTask.value)
+    }
+})
 
 // Responsável por criar as tarefas
 function createTask(text) {
     const li = createLi()
-    li.innerHTML = `<li class="list-group-item d-flex justify-content-between align-items-center">${text}</li>`
+    li.innerHTML = text
     addTask.appendChild(li)
+    inputClear()
+    createButtonDelete(li)
+    saveTask()
     
 }
 
@@ -25,7 +54,9 @@ function createTask(text) {
 //Function limpar input
 function inputClear() {
     inputTask.value = ''
+    inputTask.focus()
 }
+
 
 //captura eventos no botão Adicionar
 btnTask.addEventListener('click', function() {
@@ -39,3 +70,45 @@ btnClear.addEventListener('click', function() {
     inputClear()
 })
 
+//Captura evento ao clicar no botão excluir
+
+document.addEventListener('click', function(e) {
+    const el = e.target
+
+    if(el.classList.contains('delete-task')) {
+
+        el.parentElement.remove()
+        saveTask()
+                
+    }
+})
+
+
+//Function saveTask
+
+function saveTask() {
+    const itemTask = addTask.querySelectorAll('li')
+    const listTask = []
+
+    for (let task of itemTask ) {
+        let taskText = task.innerText
+        taskText = taskText.replace('Excluir', '').trim()
+        listTask.push(taskText)
+    }
+
+    const taskJason = JSON.stringify(listTask)
+    localStorage.setItem('task', taskJason)
+
+        
+}
+
+function readTask() {
+    const tasks = localStorage.getItem('task')
+    const listTheTask = JSON.parse(tasks)
+
+    for (let task of listTheTask) {
+        createTask(task)
+    }
+}
+
+readTask()
