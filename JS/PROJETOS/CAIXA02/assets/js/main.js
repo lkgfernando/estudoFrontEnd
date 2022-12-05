64547024724a4f655911b40eb2fd2965c371400e
Fernando Rodrigues <lkgfernando@gmail.com>
2022-12-05 13:52:27 -0300

    #

--- JS/PROJETOS/CAIXA02/assets/js/main.js
+++ JS/PROJETOS/CAIXA02/assets/js/main.js
@@ -1,89 +1,34 @@
-function creatCalculator () {
+function calculatorCashier() {
     return {
-        display: document.querySelector('.display'),
+        five_cents: document.querySelector('.five-cents'),
+        t_five_cents: document.querySelector('.tfivecents'),
 
         start() {
-            this.clickButton()
-            this.pressBackSpace()
-            this.pressEnter()
+            this.pressTab()
         },
 
-
-        //Evento de limpar display pelo backSpace
-        pressBackSpace(){
-            this.display.addEventListener('keydown', e => {
-                if(e.keyCode === 8) {
-                    e.preventDefault()
-                    this.deleteOn()
-                }
-            })
-        },
-
-        //Evento precionando enter
-        pressEnter() {
-            this.display.addEventListener('keyup', e => {
-                if(e.keyCode === 13) {
+        pressTab() {
+            this.five-cents.addEventListener('keydown', e => {
+                if(e.keyCode === 9) {
                     this.calculate()
                 }
             })
         },
 
-        //Realiza calculo
         calculate() {
-            let calculation = this.display.value
+            const cFiveCents = 0.05
+            let fiveCents = this.five_cents.value
+            let tFiveCents = fiveCents * cFiveCents
 
-            try {
-                calculation = eval(calculation)
 
-                if(!calculation) {
-                    alert('Conta inválida')
-                    return
-                }
+            this.tfivecents.value = String(tFiveCents)
 
-                this.display.value = String(calculation)
-
-            } catch (e) {
-                alert('Conta inválida')
-                return
-            }
-        },
-
-        //Limpar Display
-        clearDisplay() {
-            this.display.value = ''
-        },
-
-        deleteOn() {
-            this.display.value = this.display.value.slice(0, -1)
-        },
-
-        //Evento de click nos botões
-        clickButton() {
-            document.addEventListener('click', e => {
-                const el = e.target
-
-                if(el.classList.contains('btn-num')) {
-                    this.btnForDisplay(el.innerText)
-                }
-                if(el.classList.contains('btn-clear')) {
-                    this.clearDisplay()
-                }
-                if(el.classList.contains('btn-eq')) {
-                    this.calculate()
-                }
-                if(el.classList.contains('btn-del')) {
-                    this.deleteOn()
-                }
-
-                this.display.focus()
-            })
-        },
-
-        btnForDisplay(valor) {
-            this.display.value += valor
+        
         }
+
     }
 }
 
-const calculator = creatCalculator()
-calculator.start()
+
+const calculator = calculatorCashier()
+calculatorCashier.start()
