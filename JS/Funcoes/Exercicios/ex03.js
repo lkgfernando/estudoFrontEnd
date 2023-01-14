7bdcbb443d1b351e5f9ebb066927bf70a6c9288c
Fernando Rodrigues <lkgfernando@gmail.com>
2023-01-14 13:48:01 -0300

    #

---
+++ JS/Funcoes/Exercicios/ex03.js
@@ -0,0 +1,10 @@
+/*
+03)​ Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+*/
+
+function squareRoot(base, exponent) {
+  return console.log(base ** exponent) 
+}
+
+
+squareRoot(2,2)
