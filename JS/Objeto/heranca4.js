5d8d48b296145d4cb005543b2362cfb1e35d73c1
Fernando Rodrigues <lkgfernando@gmail.com>
2023-01-17 21:20:19 -0300

    #

--- JS/Objeto/heranca4.js
+++ JS/Objeto/heranca4.js
@@ -13,4 +13,13 @@ MeuObjeto.prototype.falar = function() {
 }
 
 obj1.falar()
+
+obj2.nome = 'Fernando'
+
 obj2.falar()
+
+const obj3 = {}
+
+obj3.__proto__ = MeuObjeto.prototype
+obj3.nome = 'Obj3'
+obj3.falar()
